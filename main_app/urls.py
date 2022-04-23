b3f1586e5a371e83a0e4d25e24019b66e0ee891a
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2022-04-23 14:44:45 -0700

    added index show

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/', views.Products_Index.as_view(), name='products_index'),
     path('products/create', views.product_create, name="product_create"),
+    # path('products/<int:pk>', views.product_show, name='product_show'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ProductCreationForm
 from .models import Product
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
@@ -15,6 +16,21 @@ def home(request):
     return render(request, 'home.html', {'context': context})
 
 
+class Products_Index(TemplateView):
+    template_name = 'products.html'
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        title = self.request.GET.get('title')
+        user = self.request.user
+        products = Product.objects.filter(user=user.id)
+        if title != None:
+            context['products'] = products.filter(name__icontains=title)
+        else:
+            context['products'] = products
+        return context
+
+
+
 def product_create(request):
     product = Product.objects.all()
     user = request.user
@@ -25,7 +41,7 @@ def product_create(request):
             product = form.save(commit=False)
             product.user = request.user 
             product.save()
-            return HttpResponseRedirect ('/product')
+            return HttpResponseRedirect ('/')
     else:
         form = form_class
     context = {
