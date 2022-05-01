5119ca94b99009984825b3abe0194086375a1116
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2022-04-30 17:37:10 -0700

    static root chagne

--- activehaul/settings.py
+++ activehaul/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
