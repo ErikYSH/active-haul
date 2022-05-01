ff94fa3a0915c46f7ab21dab8e96b5b6c088ea6a
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2022-04-30 17:27:12 -0700

    config deployment

--- activehaul/settings.py
+++ activehaul/settings.py
@@ -172,9 +172,9 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 
